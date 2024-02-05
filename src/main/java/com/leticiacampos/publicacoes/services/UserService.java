163741da68cbeeb7a9e5bce11109ae890a782c3a
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-05 12:15:40 -0300

    Obtendo um usuário por id

--- src/main/java/com/leticiacampos/publicacoes/services/UserService.java
+++ src/main/java/com/leticiacampos/publicacoes/services/UserService.java
@@ -1,6 +1,7 @@
 package com.leticiacampos.publicacoes.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.leticiacampos.publicacoes.domain.User;
 import com.leticiacampos.publicacoes.dto.UserDTO;
 import com.leticiacampos.publicacoes.repositories.UserRepository;
+import com.leticiacampos.publicacoes.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -21,5 +23,17 @@ public class UserService {
 		List<User> users = userRepository.findAll();
 		List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return usersDTO;
+	}
+	
+	public UserDTO findById(String id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		if (user.isEmpty()) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return new UserDTO(user.get());
 	}
 }
