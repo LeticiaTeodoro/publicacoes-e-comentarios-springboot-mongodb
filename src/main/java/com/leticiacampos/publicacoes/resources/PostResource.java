7cd9c087800942646b3a2f9813fbe3b80f5e3213
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-16 10:59:22 -0300

    Consulta com vários critérios

--- src/main/java/com/leticiacampos/publicacoes/resources/PostResource.java
+++ src/main/java/com/leticiacampos/publicacoes/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.leticiacampos.publicacoes.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,19 @@ public class PostResource {
 		
 		title = URL.decodeParam(title);
 		List<Post> posts = postService.findByTitle(title);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+											@RequestParam(value = "minDate", defaultValue = "") String minDate,
+											@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> posts = postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 }
