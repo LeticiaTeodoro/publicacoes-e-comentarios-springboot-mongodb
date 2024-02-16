eb77f3460faad6e7485ca2ff386986839320bc3c
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-16 10:09:20 -0300

    Adicioando comentários aos posts

--- src/main/java/com/leticiacampos/publicacoes/config/Instantiation.java
+++ src/main/java/com/leticiacampos/publicacoes/config/Instantiation.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.leticiacampos.publicacoes.domain.Post;
 import com.leticiacampos.publicacoes.domain.User;
+import com.leticiacampos.publicacoes.dto.AuthorDTO;
+import com.leticiacampos.publicacoes.dto.CommentDTO;
 import com.leticiacampos.publicacoes.repositories.PostRepository;
 import com.leticiacampos.publicacoes.repositories.UserRepository;
 
@@ -40,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", 
 					"Vou viajar para São Paulo. Abraços!", maria);
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
